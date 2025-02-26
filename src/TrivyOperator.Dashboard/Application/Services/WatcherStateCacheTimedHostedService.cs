011ec35f92a549fbe8e4ed97e0be2db3e38f9690
dradoaica <dradoaica@gmail.com>
2025-02-26 23:50:20 +0200

    Avoid using 'async' lambda when delegate type returns 'void': any exceptions unhandled by the lambda might lead to the process crash

--- src/TrivyOperator.Dashboard/Application/Services/WatcherStateCacheTimedHostedService.cs
+++ src/TrivyOperator.Dashboard/Application/Services/WatcherStateCacheTimedHostedService.cs
@@ -12,18 +12,34 @@ public class WatcherStateCacheTimedHostedService(
     IOptions<WatchersOptions> options,
     ILogger<WatcherStateCacheTimedHostedService> logger) : IHostedService, IDisposable
 {
-    private Timer? timer;
     private readonly int timeFrameInSeconds = (int)(options.Value.WatchTimeoutInSeconds * 1.1 + 60);
+    private CancellationTokenSource? stoppingCts;
+    private Timer? timer;
+    private Task? executingTask;
+    private bool disposed;
     
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        logger.LogInformation("Watcher State Cache Timed Hosted Service is running.");
-        timer = new Timer(async state => await DoWorkAsync(cancellationToken),
-            null, TimeSpan.Zero, TimeSpan.FromMinutes(1)); 
+        logger.LogInformation("Watcher State Cache Timed Hosted Service is starting.");
+        
+        stoppingCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        timer = new Timer(Execute, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+        
         return Task.CompletedTask;
     }
+    private void Execute(object? state)
+    {
+        if (executingTask == null || executingTask.IsCompleted)
+        {
+            executingTask = ExecuteAsync(stoppingCts?.Token ?? CancellationToken.None);
+        }
+        else
+        {
+            logger.LogInformation("Watcher State Cache Timed Hosted Service is still running previous execution, wait for next cycle.");
+        }
+    }
 
-    private async Task DoWorkAsync(CancellationToken cancellationToken)
+    private async Task ExecuteAsync(CancellationToken cancellationToken)
     {
         try
         {
@@ -47,20 +63,48 @@ public class WatcherStateCacheTimedHostedService(
         }
         catch(Exception ex)
         {
-            logger.LogError(ex, "Crashed.");
+            logger.LogError(ex, "Watcher State Cache Timed Hosted Service execution has crashed.");
         }
     }
 
-    public Task StopAsync(CancellationToken cancellationToken)
+    public async Task StopAsync(CancellationToken cancellationToken)
     {
-        logger.LogInformation("Timed Hosted Service is stopping.");
+        logger.LogInformation("Watcher State Cache Timed Hosted Service is stopping.");
+        
         timer?.Change(Timeout.Infinite, 0);
-        return Task.CompletedTask;
+        
+        if (executingTask == null || executingTask.IsCompleted)
+        {
+            return;
+        }
+
+        try
+        {
+            stoppingCts!.Cancel();
+        }
+        finally
+        {
+            await executingTask.WaitAsync(cancellationToken).ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
+        }
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                timer?.Dispose();
+                stoppingCts?.Cancel();
+            }
+
+            disposed = true;
+        }
     }
 
     public void Dispose()
     {
-        timer?.Dispose();
+        Dispose(true);
+        
         GC.SuppressFinalize(this);
     }
 }
