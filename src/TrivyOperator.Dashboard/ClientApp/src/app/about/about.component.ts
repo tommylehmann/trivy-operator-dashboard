bb1e39e47a6790d50aae1c0b272c6c164e92e056
raoulx24 <raoulx24@yahoo.com>
2025-04-16 18:14:31 +0300

    about - added otel, changed credits to []

--- src/TrivyOperator.Dashboard/ClientApp/src/app/about/about.component.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/about/about.component.ts
@@ -10,6 +10,7 @@ import { TagModule} from 'primeng/tag'
 import { AppVersionService } from '../../api/services/app-version.service';
 import { GitHubRelease } from '../../api/models/git-hub-release';
 import { AppVersion } from '../../api/models/app-version'
+import { AboutCredits } from './about.types';
 
 @Component({
   selector: 'app-about',
@@ -23,6 +24,34 @@ export class AboutComponent {
   releaseNotes?: GitHubRelease[];
   currentVersion?: AppVersion;
   newVersionAvailable: boolean = false;
+
+  credits: AboutCredits[] = [
+    {
+      name: 'Trivy Operator', imgSrc: 'assets/trivy-operator-logo.png', imgAlt: 'Trivy Operator Logo', imgExtraStyle: 'filter: invert(1)',
+      homeUrl: 'https://trivy.dev/latest/', gitUrl: 'https://github.com/aquasecurity/trivy-operator',
+      docsUrl: 'https://aquasecurity.github.io/trivy-operator/latest/'
+    },
+    {
+      name: '.net 8', imgSrc: 'assets/net8.png', imgAlt: 'dot net 8 Logo',
+      homeUrl: 'https://dotnet.microsoft.com/en-us/', gitUrl: 'https://github.com/dotnet/core',
+      docsUrl: 'https://learn.microsoft.com/en-us/dotnet/'
+    },
+    {
+      name: 'Angular', imgSrc: 'assets/angular-js.png', imgAlt: 'Angular 18 Logo',
+      homeUrl: 'https://angular.dev/', gitUrl: 'https://github.com/angular/angular',
+      docsUrl: 'https://angular.dev/overview'
+    },
+    {
+      name: 'PrimeNG', imgSrc: 'assets/primeng.png', imgAlt: 'PrimeNG Logo',
+      homeUrl: 'https://primeng.org/', gitUrl: 'https://github.com/primefaces/primeng',
+      docsUrl: 'https://primeng.org/installation'
+    },
+    {
+      name: 'OpenTelemtry', imgSrc: 'assets/opentelemetry.png', imgAlt: 'OpenTelemetry Logo',
+      homeUrl: 'https://opentelemetry.io/', gitUrl: 'https://github.com/open-telemetry',
+      docsUrl: 'https://opentelemetry.io/docs/'
+    }
+  ];
 
   constructor(private service: AppVersionService) {
     this.getReleaseNotesDtos();
---
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/about/about.types.ts
@@ -0,0 +1,10 @@
+export interface AboutCredits {
+  name: string;
+  imgSrc: string;
+  imgAlt: string;
+  imgExtraStyle?: string;
+  homeUrl: string;
+  gitUrl: string;
+  docsUrl: string;
+  
+}
