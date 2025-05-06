a74c99b248cb4ee49e13fef7c64539eaf0b82637
raoulx24 <raoulx24@yahoo.com>
2025-05-06 23:47:16 +0300

    some bug fixes in darkmode service

--- src/TrivyOperator.Dashboard/ClientApp/src/app/services/dark-mode.service.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/services/dark-mode.service.ts
@@ -21,45 +21,43 @@ export class DarkModeService {
     return this.isDarkModeSubject.getValue();
   }
 
-  private setDarkMode(value: boolean, isUserEvent: boolean) {
-    this.isDarkMode = value;
-    this.isDarkModeSubject.next(value);
-    if (isUserEvent) {
-      localStorage.setItem(this.locaStorageIsDarkModeKey, value.toString());
-    }
-    else {
-      const savedTheme = LocalStorageUtils.getBoolKeyValue(this.locaStorageIsDarkModeKey);
-      if (savedTheme !== null) {
-        return;
-      }
-    }
-
-    const root = document.documentElement;
-    if (value) {
-      root.classList.add(DarkModeService.DARK_MODE_SELECTOR);
-    } else {
-      root.classList.remove(DarkModeService.DARK_MODE_SELECTOR);
-    }
-  }
-
   public restoreMode() {
     const savedTheme = LocalStorageUtils.getBoolKeyValue(this.locaStorageIsDarkModeKey);
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    this.setDarkMode(savedTheme ?? prefersDarkMode, false);
+    this.setMode(savedTheme ?? prefersDarkMode);
   }
 
   public toggleDarkMode() {
-    this.setDarkMode(!this.darkMode, true);
+    localStorage.setItem(this.locaStorageIsDarkModeKey, (!this.darkMode).toString());
+    this.setMode(!this.darkMode);
   }
 
   private watchSystemDarkMode() {
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     mediaQuery.addEventListener('change', (event) => {
-      const newIsDarkMode = event.matches;
-      if (this.isDarkMode === newIsDarkMode) {
+      const savedTheme = LocalStorageUtils.getBoolKeyValue(this.locaStorageIsDarkModeKey);
+      if (savedTheme !== null) {
+        // if manual set (key exists in localstorage), exit
         return;
       }
-      this.setDarkMode(newIsDarkMode, false);
+      const newIsDarkMode = event.matches;
+      if (this.isDarkMode === newIsDarkMode) {
+        // ignore duplicate events
+        return;
+      }
+      this.setMode(newIsDarkMode);
     });
+  }
+
+  private setMode(darkMode: boolean) {
+    this.isDarkMode = darkMode;
+    this.isDarkModeSubject.next(darkMode);
+    const root = document.documentElement;
+    if (darkMode) {
+      root.classList.add(DarkModeService.DARK_MODE_SELECTOR);
+    }
+    else {
+      root.classList.remove(DarkModeService.DARK_MODE_SELECTOR);
+    }
   }
 }
