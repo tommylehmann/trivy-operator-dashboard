2ad9e73b083cd126c5eeb07615770ef661f543ae
raoulx24 <raoulx24@yahoo.com>
2025-05-25 01:55:06 +0300

    NamespacedResourceDomainService - back to async + removal of useless try catch

--- src/TrivyOperator.Dashboard/Domain/Services/Abstractions/NamespacedResourceDomainService.cs
+++ src/TrivyOperator.Dashboard/Domain/Services/Abstractions/NamespacedResourceDomainService.cs
@@ -15,36 +15,27 @@ public abstract class NamespacedResourceDomainService<TKubernetesObject, TKubern
 {
     public override async Task<IList<TKubernetesObject>> GetResources(CancellationToken? cancellationToken = null)
     {
-        try
+        IEnumerable<V1Namespace> v1Namespaces = await namespaceDomainService.GetResources(cancellationToken);
+        List<TKubernetesObject> trivyReports = [];
+        foreach (V1Namespace? v1Namespace in v1Namespaces ?? [])
         {
-            IEnumerable<V1Namespace> v1Namespaces = await namespaceDomainService.GetResources(cancellationToken);
-            List<TKubernetesObject> trivyReports = [];
-            foreach (V1Namespace? v1Namespace in v1Namespaces ?? [])
+            IList<TKubernetesObject> trivyReportsInNamespace =
+                await GetResources(v1Namespace.Name(), cancellationToken);
+            if (cancellationToken is { IsCancellationRequested: true })
             {
-                IList<TKubernetesObject> trivyReportsInNamespace =
-                    await GetResources(v1Namespace.Name(), cancellationToken);
-                if (cancellationToken is { IsCancellationRequested: true })
-                {
-                    return [];
-                }
-
-                trivyReports.AddRange(trivyReportsInNamespace);
+                return [];
             }
 
-            return trivyReports;
+            trivyReports.AddRange(trivyReportsInNamespace);
         }
-        catch { throw; }
+
+        return trivyReports;
     }
 
-    public async Task<IList<TKubernetesObject>> GetResources(
-        string namespaceName, CancellationToken? cancellationToken = null)
+    public async Task<IList<TKubernetesObject>> GetResources(string namespaceName, CancellationToken? cancellationToken = null)
     {
-        try
-        {
-            TKubernetesObjectList kubernetesObjectList = await GetResourceList(namespaceName, cancellationToken: cancellationToken);
-            return kubernetesObjectList.Items;
-        }
-        catch { throw; }
+        TKubernetesObjectList kubernetesObjectList = await GetResourceList(namespaceName, cancellationToken: cancellationToken);
+        return kubernetesObjectList.Items;
     }
 
     public abstract Task<TKubernetesObjectList> GetResourceList(
