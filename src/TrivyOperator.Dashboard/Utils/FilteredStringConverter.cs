8973ba8cdbe6469334723dcd9ba11aac33c4bd9c
raoulx24 <raoulx24@yahoo.com>
2025-06-18 22:34:51 +0300

    FilteredStringConverter for System.Text.Json

---
+++ src/TrivyOperator.Dashboard/Utils/FilteredStringConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TrivyOperator.Dashboard.Utils;
+
+public class FilteredStringConverter(string filter, string filterWith) : JsonConverter<string>
+{
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.GetString() ?? string.Empty;
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+    {
+        string? filtered = value?.Replace(filter, filterWith);
+        writer.WriteStringValue(filtered);
+    }
+}
