15017e52d69da03bf2f5d4c0d01ad919f3f39ad4
dradoaica <dradoaica@gmail.com>
2025-07-14 00:00:10 +0300

    Constants.USER_AGENT_NAME

--- src/TrivyOperator.Dashboard/Constants.cs
+++ src/TrivyOperator.Dashboard/Constants.cs
@@ -2,5 +2,5 @@
 
 public static class Constants
 {
-    public const string USER_AGENT_NAME = "Trivy.Operator.Dashboard/1.7";
+    public const string USER_AGENT_NAME = "TrivyOperator.Dashboard/1.7";
 }
